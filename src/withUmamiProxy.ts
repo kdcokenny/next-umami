26a57340726cf29b10be3d08418e53fcc938f050
Kenny <kenny@kdco.llc>
2024-11-10 16:45:38 -0500

    chore: withUmamiProxy fix

--- src/withUmamiProxy.ts
+++ src/withUmamiProxy.ts
@@ -1,33 +1,32 @@
-import type { NextConfig } from 'next'
+import { NextConfig } from 'next'
 
-export default function withUmamiProxy() {
-  ;(nextConfig: NextConfig): NextConfig => {
-    return {
-      rewrites: async () => {
-        const umamiRewrites = [
-          {
-            source: 'https://cloud.umami.is/script.js',
-            destination: '/script.js',
-          },
-        ]
-        if (process.env.NEXT_PLAUSIBLE_DEBUG) {
-          console.log('umamiRewrites = ', umamiRewrites)
-        }
+export default function withUmamiProxy(nextConfig: NextConfig): NextConfig {
+  return {
+    ...nextConfig,
+    rewrites: async () => {
+      const umamiRewrites = [
+        {
+          source: 'https://cloud.umami.is/script.js',
+          destination: '/script.js',
+        },
+      ]
+      if (process.env.NEXT_PLAUSIBLE_DEBUG) {
+        console.log('umamiRewrites = ', umamiRewrites)
+      }
 
-        const rewrites = await nextConfig.rewrites?.()
+      const rewrites = await nextConfig.rewrites?.()
 
-        if (!rewrites) {
-          return umamiRewrites
-        } else if (Array.isArray(rewrites)) {
-          return rewrites.concat(umamiRewrites)
-        } else if (rewrites.afterFiles) {
-          rewrites.afterFiles = rewrites.afterFiles.concat(umamiRewrites)
-          return rewrites
-        } else {
-          rewrites.afterFiles = umamiRewrites
-          return rewrites
-        }
-      },
-    }
+      if (!rewrites) {
+        return umamiRewrites
+      } else if (Array.isArray(rewrites)) {
+        return rewrites.concat(umamiRewrites)
+      } else if (rewrites.afterFiles) {
+        rewrites.afterFiles = rewrites.afterFiles.concat(umamiRewrites)
+        return rewrites
+      } else {
+        rewrites.afterFiles = umamiRewrites
+        return rewrites
+      }
+    },
   }
 }
