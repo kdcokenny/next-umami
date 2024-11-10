64e170e136e5c447d7b792501407a8f56ddac39f
Kenny <kenny@kdco.llc>
2024-11-10 16:38:47 -0500

    chore: expose `withUmamiProxy`

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import useUmami from './src/useUmami'
+import withUmamiProxy from './src/withUmamiProxy'
 import UmamiProvider from './src/UmamiProvider'
 
-export { useUmami }
+export { useUmami, withUmamiProxy }
 export default UmamiProvider
